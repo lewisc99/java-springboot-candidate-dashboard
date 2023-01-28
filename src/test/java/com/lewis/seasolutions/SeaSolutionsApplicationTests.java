bc11acada861becf6f12b1ec975a68c62c192b87
lewisc99 <lewiscontato99@gmail.com>
2023-01-28 20:52:15 -0300

    created project

---
+++ src/test/java/com/lewis/seasolutions/SeaSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewis.seasolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
