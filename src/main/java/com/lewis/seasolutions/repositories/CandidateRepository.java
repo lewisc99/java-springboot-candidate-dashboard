ca5c4c70098e95ab75ad7398a61d265a723ae13e
lewisc99 <lewiscontato99@gmail.com>
2023-01-28 23:23:12 -0300

    created repository CandidateRepository

--- src/main/java/com/lewis/seasolutions/repositories/CandidateRepository.java
+++ src/main/java/com/lewis/seasolutions/repositories/CandidateRepository.java
@@ -1,8 +1,10 @@
 package com.lewis.seasolutions.repositories;
 
 
+import jdk.jfr.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CandidateRepository{
+public interface CandidateRepository extends JpaRepository<Category, Long> {
 }
